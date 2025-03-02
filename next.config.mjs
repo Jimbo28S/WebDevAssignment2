ee1da43855d740834c365824673db7ab36deebbd
James Smith <157991703+Jimbo28S@users.noreply.github.com>
2025-03-01 19:21:41 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === 'production';
+const nextConfig = {reactStrictMode: true,
+    images: {
+      unoptimized: true, // Disable default image optimization
+    },
+    assetPrefix: isProd ? '/WebDevAssignment2/' : '',
+    basePath: isProd ? '/WebDevAssignment2' : '',
+    output: 'export'
+  };
 
 export default nextConfig;
